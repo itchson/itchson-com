34ba3cbf70378a02eaeb19cbbaa355dc1602bbc4
Itchson <mitchowr@gmail.com>
2023-02-28 20:51:35 +1000

    added grid for mobile view

--- src/App.js
+++ src/App.js
@@ -7,30 +7,18 @@ import Blog from './pages/Blog';
 import Faq from './pages/Faq';
 import './App.css';
 
-const GridContainer = styled.div`
-  display: grid;
-  overflow: hidden;
-  grid-template-areas: 
-    "nav article";
-  grid-template-rows: 100% 1fr 70px;  
-  grid-template-columns: 200px 1fr 0%;
-  height: 100vh;
-  margin: 0;
-`;
-
 const Nav = styled.nav`
   grid-area: nav;
 `;
 
 const Article = styled.article`
   grid-area: article;
-  overflow-y: scroll;
 `;
 
 function App() {
   return (
     <Router>
-      <GridContainer>
+      <div className="App">
         <Nav>
           <Sidebar />
         </Nav>
@@ -41,7 +29,7 @@ function App() {
             <Route path="/faq" element={<Faq />} />
           </Routes>
         </Article>
-      </GridContainer>
+      </div>
     </Router>
   );
 }
